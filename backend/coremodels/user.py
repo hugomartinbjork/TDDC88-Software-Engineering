27ba7ec88d010036ce71c84fc55574a1d17783a5
Martin <marho558@student.liu.se>
2022-09-27 16:20:34 +0200

    Cost center and user class added

--- backend/coremodels/user.py
+++ backend/coremodels/user.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import
+from django.contrib.auth.models import User
 
 
 class User(models.Model):
-    username = models.CharField(max_length=30)
-    password_id = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     cost_center = models.CharField(max_length=30)
-    roles =
+
+    def __str__(self):
+        return self.user
