1b6ada1080443855d97b44047b6e237599cbfbbd
Axel <axejo347@student.liu.se>
2022-09-27 14:40:45 +0200

    A functional, but not done article class

--- backend/coremodels/article.py
+++ backend/coremodels/article.py
@@ -3,5 +3,14 @@ from django.db import models
 class Article(models.Model):
     name = models.CharField(max_length=30)
     lioId = models.CharField(max_length=15, primary_key=True)
+    image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=100, null = True)
+    price = models.IntegerField(null = True)
+    refill_unit = models.IntegerField(null = True)
+    take_out_unit = models.IntegerField(null = True)
+    alternative_names = models.TextField(null=True, blank=True)
+    alternative_articles = models.ForeignKey('self', on_delete=models.CASCADE, blank = True, null=True)
+    supplier = models.CharField(max_length=30, null = True)
+    sup_ordernr = models.IntegerField(null = True)
     def __str__(self):
         return self.name
--- backend/serializers.py
+++ backend/serializers.py
@@ -6,7 +6,7 @@ from backend.coremodels.storageComponent import storageUnit
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('name', 'lioId')
+        fields = ('name', 'lioId', 'image', 'description', 'price', 'refill_unit', 'take_out_unit', 'alternative_names', 'alternative_articles', 'supplier', 'sup_ordernr')
 
 class StorageSerializer(serializers.ModelSerializer):
     class Meta:
