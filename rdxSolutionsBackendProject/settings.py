47006ac77fbeb66dbfaeb126d21951efb11dcb86
Elias Leijonmarck <elile642@student.liu.se>
2022-09-27 17:18:07 +0200

    Added non-pw protected version

--- rdxSolutionsBackendProject/settings.py
+++ rdxSolutionsBackendProject/settings.py
@@ -75,13 +75,28 @@ WSGI_APPLICATION = 'rdxSolutionsBackendProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'rdxSolutionsDatabase',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Kebab100',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
-
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'OPTIONS': {
+        #     'service': 'my_service',
+        #     'passfile': '.my_pgpass',
+        # },
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
