c438b78b8cfbec431f588db5444a8e7e8d682be3
Axel <axejo347@student.liu.se>
2022-09-28 12:42:28 +0200

    Currently working, but have issues with the relationship between article and group

--- backend/coremodels/article.py
+++ backend/coremodels/article.py
@@ -1,16 +1,33 @@
 from django.db import models
+from backend.coremodels.group import Group
+
+
 
 class Article(models.Model):
     name = models.CharField(max_length=30)
     lioId = models.CharField(max_length=15, primary_key=True)
+    article_group = models.ManyToManyField(Group)
     image = models.ImageField(null=True, blank=True)
     description = models.CharField(max_length=100, null = True)
+    std_cost = models.IntegerField(null = True)
+    minimal_order_qt = models.IntegerField(null = True)
     price = models.IntegerField(null = True)
     refill_unit = models.IntegerField(null = True)
     take_out_unit = models.IntegerField(null = True)
     alternative_names = models.TextField(null=True, blank=True)
-    alternative_articles = models.ForeignKey('self', on_delete=models.CASCADE, blank = True, null=True)
+    alternative_articles = models.ManyToManyField('self', blank = True)
     supplier = models.CharField(max_length=30, null = True)
     sup_ordernr = models.IntegerField(null = True)
+
     def __str__(self):
         return self.name
+
+
+
+
+
+
+
+
+
+
--- backend/serializers.py
+++ backend/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from backend.coremodels.article import Article
+from backend.coremodels.article import Group
 from backend.coremodels.storage import Storage
 from backend.coremodels.storageComponent import storageUnit
+
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('name', 'lioId', 'image', 'description', 'price', 'refill_unit', 'take_out_unit', 'alternative_names', 'alternative_articles', 'supplier', 'sup_ordernr')
+        fields = ('name', 'lioId', 'article_group', 'image', 'description', 'std_cost', 'minimal_order_qt', 'price', 'refill_unit', 'take_out_unit', 'alternative_names',
+         'alternative_articles', 'supplier', 'sup_ordernr')
 
 class StorageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +20,8 @@ class StorageUnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = storageUnit
         fields = ('currentStock', 'article', 'storage',)
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'group_name')
