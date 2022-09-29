e6733a3f2cba88273172760e1b3336a32d43db4e
Martin <marho558@student.liu.se>
2022-09-29 15:18:58 +0200

    Cost center functionality added to user model

--- backend/admin.py
+++ backend/admin.py
@@ -4,10 +4,23 @@ from backend.coremodels.storage import Storage
 from backend.coremodels.storageComponent import storageUnit
 from backend.coremodels.cost_center import CostCenter
 from backend.coremodels.user_info import UserInfo
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Storage)
 admin.site.register(storageUnit)
 admin.site.register(CostCenter)
-admin.site.register(UserInfo)
+
+
+class UserInfoInLine(admin.StackedInline):
+    model = UserInfo
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserInfoInLine, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
