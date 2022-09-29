66674250fe5e3773f7be6bcbe8a811e1d3068052
Frippen <freha662@student.liu.se>
2022-09-29 15:11:44 +0200

    reverted back to SQLite

--- rdxSolutionsBackendProject/settings.py
+++ rdxSolutionsBackendProject/settings.py
@@ -73,34 +73,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rdxSolutionsBackendProject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ # Database
+ # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : databaseConfig.my_Databaseparameters['NAME'],
-        'USER' : 'postgres',
-        'PASSWORD' : databaseConfig.my_Databaseparameters['PASSWORD'],
-        'HOST': 'localhost', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'OPTIONS': {
-        #     'service': 'my_service',
-        #     'passfile': '.my_pgpass',
-        # },
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
