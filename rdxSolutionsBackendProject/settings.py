38c6bb7b723491af37b0c6be03f723e2ef671bf7
Hugo Bjork <hugobjork@me.com>
2022-09-30 13:13:27 +0200

    login

--- rdxSolutionsBackendProject/settings.py
+++ rdxSolutionsBackendProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rdxSolutionsBackendProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "rdxSolutionsBackendProject/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/success"
