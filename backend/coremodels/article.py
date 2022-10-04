6e564e5841d572276845f05fa238baf14247134b
rickardpeters <77685817+rickardpeters@users.noreply.github.com>
2022-10-04 11:55:28 +0200

    changed all the class names and variable names to match the database schema

--- backend/coremodels/article.py
+++ backend/coremodels/article.py
@@ -7,21 +7,29 @@ from backend.coremodels.group import GroupInfo
 
 
 class Article(models.Model):
-    name = models.CharField(max_length=30)
     lioId = models.CharField(max_length=15, primary_key=True)
-    article_group = models.ManyToManyField(GroupInfo)
-    image = models.ImageField(null=True, blank=True)
     description = models.CharField(max_length=100, null = True)
-    std_cost = models.IntegerField(null = True)
-    minimal_order_qt = models.IntegerField(null = True)
     price = models.IntegerField(null = True)
-    refill_unit = models.IntegerField(null = True)
-    take_out_unit = models.IntegerField(null = True)
-    alternative_names = models.TextField(null=True, blank=True)
-    alternative_articles = models.ManyToManyField('self', blank = True)
-    supplier = models.CharField(max_length=30, null = True)
-    sup_ordernr = models.IntegerField(null = True)
-   # storageComponent = models.CharField(max_length=15, null = True)
+    name = models.CharField(max_length=30)
+    sanitation_level = models.CharField(max_length=30, default=0)
+    image = models.ImageField(null=True, blank=True)
+    
+
+    ## Should these attributes below really be included, they are not
+    # included in the database schema??
+
+    article_group = models.ManyToManyField(GroupInfo)
+
+
+#     std_cost = models.IntegerField(null = True)
+#     minimal_order_qt = models.IntegerField(null = True)
+#     refill_unit = models.IntegerField(null = True)
+#     take_out_unit = models.IntegerField(null = True)
+#     alternative_names = models.TextField(null=True, blank=True)
+#     alternative_articles = models.ManyToManyField('self', blank = True)
+#     supplier = models.CharField(max_length=30, null = True)
+#     sup_ordernr = models.IntegerField(null = True)
+#    # storageComponent = models.CharField(max_length=15, null = True)
 
     def __str__(self):
         return self.name
