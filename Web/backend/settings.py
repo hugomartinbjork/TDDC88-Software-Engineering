89dbfaf6940a7658b6882dfb4fe9a8d995b34b4f
Gustav Kindgren <guski416@student.liu.se>
2022-10-11 11:53:13 +0000

    Update Web/backend/settings.py

--- Web/backend/settings.py
+++ Web/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&kgxky3c#god85vjz8-%ygle2^)iac+unxt_qud^tp4(10hi&_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
