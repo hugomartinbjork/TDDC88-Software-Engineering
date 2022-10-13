af7aa75fe3e4361bee1fce6a6139a8b412021c67
guski416 <guski416@tlvm-4-1-4.ad.liu.se>
2022-10-13 12:36:04 +0200

    change in wsgi file for testing purposes

--- Web/rdxSolutionsBackendProject/wsgi.py
+++ Web/rdxSolutionsBackendProject/wsgi.py
@@ -7,18 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
-import pathlib
-
-
 from django.core.wsgi import get_wsgi_application
-
-
-
-CURRENT_DIR = pathlib.Path(__file__).resolve().parent
-BASE_DIR = CURRENT_DIR.parent
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rdxSolutionsBackendProject.settings')
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
