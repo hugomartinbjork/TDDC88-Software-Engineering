235c36e4d4a0b95247fc26f462123af3e512d9f0
guski416 <guski416@tlvm-4-1-4.ad.liu.se>
2022-10-13 11:28:29 +0200

    trying again

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -2,5 +2,4 @@
 APP_PORT=${PORT:-8000}
 cd /app/
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
-/opt/venv/bin/python3 manage.py collectstatic --noinput
-/opt/venv/bin/python3 manage.py migrate
+/opt/venv/bin/python3 manage.py collectstatic --noinput && /opt/venv/bin/python3 manage.py migrate
