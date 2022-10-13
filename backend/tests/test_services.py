2ef227c8c5abed696832570ac9d5e32892db20c4
Gustav Nordlander <gusno450@student.liu.se>
2022-10-13 11:17:37 +0200

    FR4.6 'done'

--- backend/tests/test_services.py
+++ backend/tests/test_services.py
@@ -1,13 +1,32 @@
 from django.test import TestCase
-from backend.coremodels.article import * 
+from backend.coremodels.article import *
+from backend.coremodels.storage_space import StorageSpace
+from backend.coremodels.storage_unit import StorageUnit 
 from backend.services.articleManagementService import *
+from backend.services.storageManagementService import storageManagementService
 
 # Testing FR4.1
 class ArticleIdentificationTest(TestCase):    
      def setUp(self):                     
          Article.objects.create(lioId="1")         
             
-         
      def test_getArticleByLioId_function(self): 
         article = Article.objects.get(lioId="1")                  
         self.assertEqual(articleManagementService.getArticleByLioId(self,"1"), article)        
+
+# Testing FR4.6
+class StorageSpaceCreationTest(TestCase):    
+     def setUp(self):                     
+         Article.objects.create(lioId="1")
+         StorageUnit.objects.create(id="1")
+         StorageSpace.objects.create(id="1", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"))
+            
+     def test_storageManagementService(self):
+        storageunit = StorageUnit.objects.get(id="1")
+        storagespace = StorageSpace.objects.get(id="1")
+        article1 = Article.objects.get(lioId="1")
+        self.assertEqual(articleManagementService.getArticleByLioId(self,"1"), article1)
+        self.assertEqual(storageManagementService.getStorageUnitById(self,"1"), storageunit)
+        self.assertEqual(storageManagementService.getStorageSpaceById(self,"1"), storagespace)
+        self.assertEqual(storagespace.article, article1)
+        self.assertEqual(storagespace.storage_unit, storageunit)
