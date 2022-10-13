b4936acdf9d017f31f954501c43280383fb0a18a
axeek668 <axeek668@student.liu.se>
2022-10-13 14:22:27 +0200

    remove venv

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-APP_PORT=${PORT:-8000}
+#APP_PORT=${PORT:-8000}
 cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+#/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
 
-/opt/venv/bin/python3 manage.py migrate
-/opt/venv/bin/python3 manage.py runserver
-/opt/venv/bin/python3 manage.py collectstatic --noinput 
+python3 manage.py migrate
+python3 manage.py runserver
+python3 manage.py collectstatic --noinput 
