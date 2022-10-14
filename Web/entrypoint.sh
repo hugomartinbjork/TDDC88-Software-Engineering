c7627df33e5e5f36aecdda831e44f89114b61cb7
guski416 <guski416@tlvm-4-2-3.ad.liu.se>
2022-10-14 18:26:19 +0200

    small adjustment in entrypoint.sh

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@rdx.com"
 APP_PORT=${PORT:-8000}
 cd /app/
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
 /opt/venv/bin/python3 manage.py migrate --noinput
 /opt/venv/bin/python3 manage.py collectstatic --noinput
-/opt/venv/bin/python manage.py createsuperuser --noinput || true
+/opt/venv/bin/python3 manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
