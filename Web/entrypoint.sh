67ff5ac8379d8fb35fae2f778f9e10cda8eb7c21
guski416 <guski416@tlvm-4-2-3.ad.liu.se>
2022-10-14 21:02:06 +0200

    apparrently can'tremove the environment variables

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -4,5 +4,5 @@ cd /app/
 # /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
 /opt/venv/bin/python3 manage.py migrate --noinput
 /opt/venv/bin/python3 manage.py collectstatic --noinput
-/opt/venv/bin/python3 manage.py createsuperuser --email "admin@rdx.com" --noinput || true
+/opt/venv/bin/python3 manage.py createsuperuser --noinput || true
 /opt/venv/bin/python3 manage.py runserver 0.0.0.0:8000
