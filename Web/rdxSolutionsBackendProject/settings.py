274882b2c022677143d0222632466f777a3eb0b7
guski416 <guski416@tlvm-4-2-3.ad.liu.se>
2022-10-14 19:45:49 +0200

    added django_secret_key as a environmental variable

--- Web/rdxSolutionsBackendProject/settings.py
+++ Web/rdxSolutionsBackendProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&kgxky3c#god85vjz8-%ygle2^)iac+unxt_qud^tp4(10hi&_'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
