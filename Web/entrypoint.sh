d66e58d78f5fc7c32025d06ae4192c87b75aeab0
guski416 <guski416@tlvm-4-4-2.ad.liu.se>
2022-10-14 11:45:44 +0200

    reset so the website runs

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#APP_PORT=${PORT:-8000}
-cd ..
-#/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
-
-opt/venv/bin/python3 manage.py migrate
-opt/venv/bin/python3 manage.py collectstatic --noinput
-opt/venv/bin/python3 manage.py runserver 0.0.0.0:8000
+APP_PORT=${PORT:-8000}
+cd /app/
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm rdxSolutionsBackendProject.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/python3 manage.py migrate
+/opt/venv/bin/python3 manage.py collectstatic --noinput
+/opt/venv/bin/python manage.py createsuperuser --noinput || true
