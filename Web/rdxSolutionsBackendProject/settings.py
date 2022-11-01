cacf53ec2af6b1ad57373e612220f36d0dfb991b
Gustav Kindgren <guski416@student.liu.se>
2022-11-01 07:05:28 +0000

    Update Web/rdxSolutionsBackendProject/settings.py

--- Web/rdxSolutionsBackendProject/settings.py
+++ Web/rdxSolutionsBackendProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&kgxky3c#god85vjz8-%ygle2^)iac+unxt_qud^tp4(10hi&_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
