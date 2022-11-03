bc4cb13b400105cbb7c484b44e676abfd118f3a5
guski416 <guski416@tlvm-4-2-3.ad.liu.se>
2022-11-03 12:36:58 +0100

    added back relevant installed apps which had dissapeared

--- Web/rdxSolutionsBackendProject/settings.py
+++ Web/rdxSolutionsBackendProject/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'backend',
+    'knox',
+   
 ]
 
 REST_FRAMEWORK = {
