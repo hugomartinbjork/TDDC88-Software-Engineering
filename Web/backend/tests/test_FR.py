b86c255add6666b161eb81a6fe7ce668125036ba
Gustav Nordlander <gusno450@student.liu.se>
2022-11-07 14:49:20 +0100

    refactored testfiles and solved migrationissues

--- Web/backend/tests/test_FR.py
+++ Web/backend/tests/test_FR.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from backend.dataAccess.articleAccess import articleAccess
+from backend.dataAccess.articleAccess import ArticleAccess
 from backend.coremodels.article import Article
-from backend.coremodels.storage_space import StorageSpace
-from backend.coremodels.storage_unit import StorageUnit 
+from backend.coremodels.compartment import Compartment
+from backend.coremodels.storage import Storage 
 from backend.coremodels.qr_code import QRCode
-from backend.services.articleManagementService import articleManagementService
-from backend.services.storageManagementService import storageManagementService
+from backend.services.articleManagementService import ArticleManagementService
+from backend.services.storageManagementService import StorageManagementService
 import unittest
 from unittest.mock import MagicMock
 
@@ -18,67 +18,67 @@ from unittest.mock import MagicMock
 # Run this test individually with the command: python manage.py test backend.tests.test_FR.ArticleIdentificationTest
 class ArticleIdentificationTest(TestCase):
     def setUp(self):
-        self.article_to_search_for = Article.objects.create(lioId ="1") #Database is populated and the object is stored so that we don't have to retrieve it again
-        self.article_management_service : articleManagementService = articleManagementService() #An instance of the class to be tested is created and stored as a class variable for the test class. The "articleManagementService :" part specifies that the stored variable must be of type articlemanagementservice, this is not necessary, but makes the code more understandable
+        self.article_to_search_for = Article.objects.create(lio_id ="1") #Database is populated and the object is stored so that we don't have to retrieve it again
+        self.article_management_service : ArticleManagementService = ArticleManagementService() #An instance of the class to be tested is created and stored as a class variable for the test class. The "articleManagementService :" part specifies that the stored variable must be of type articlemanagementservice, this is not necessary, but makes the code more understandable
     def test_get_article_by_lio_id(self):
-        test_search = self.article_management_service.getArticleByLioId("1")
+        test_search = self.article_management_service.get_article_by_lio_id("1")
         self.assertEqual(test_search, self.article_to_search_for)
 
 # === Previously written like this, use for reference when rewriting other tests === #
 # class ArticleIdentificationTest(TestCase):    
 #      def setUp(self):                     
-#          Article.objects.create(lioId="1")         
+#          Article.objects.create(lio_id="1")         
             
-#      def test_getArticleByLioId_function(self): 
-#         article = Article.objects.get(lioId="1")                  
-#         self.assertEqual(articleManagementService.getArticleByLioId(self,"1"), article)        
+#      def test_get_article_by_lio_id_function(self): 
+#         article = Article.objects.get(lio_id="1")                  
+#         self.assertEqual(articleManagementService.get_article_by_lio_id(self,"1"), article)        
 
 
 # Testing FR4.6
-class StorageSpaceCreationTest(TestCase):    
+class CompartmentCreationTest(TestCase):    
      def setUp(self):                     
-         self.article_in_storagespace = Article.objects.create(lioId="1")
-         self.article_management_service : articleManagementService = articleManagementService()
-         self.storageunit_in_storagespace = StorageUnit.objects.create(id="1")
-         self.storage_management_service : storageManagementService = storageManagementService()
-         self.storagespace = StorageSpace.objects.create(id="1", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"))
+         self.article_in_compartment = Article.objects.create(lio_id="1")
+         self.article_management_service : ArticleManagementService = ArticleManagementService()
+         self.storage_in_compartment = Storage.objects.create(id="1")
+         self.storage_management_service : StorageManagementService = StorageManagementService()
+         self.compartment = Compartment.objects.create(id="1", storage = Storage.objects.get(id="1"), article = Article.objects.get(lio_id="1"))
             
      def test_storageManagementService(self):
-        test_search_storagespace = self.storage_management_service.getStorageSpaceById(id="1")
-        test_search_article = self.article_management_service.getArticleByLioId(lioId="1")
-        test_search_storageunit = self.storage_management_service.getStorageUnitById(id="1")
-        self.assertEqual(test_search_storagespace, self.storagespace)
-        self.assertEqual(self.storagespace.article, test_search_article)
-        self.assertEqual(self.storagespace.storage_unit, test_search_storageunit)
+        test_search_compartment = self.storage_management_service.get_compartment_by_id(id="1")
+        test_search_article = self.article_management_service.get_article_by_lio_id(lio_id="1")
+        test_search_storage = self.storage_management_service.get_storage_by_id(id="1")
+        self.assertEqual(test_search_compartment, self.compartment)
+        self.assertEqual(self.compartment.article, test_search_article)
+        self.assertEqual(self.compartment.storage, test_search_storage)
 
 
 # Testing FR4.3
 class FR4_3_Test(TestCase):
     def setUp(self):
         #create 2 storage spaces in the same storage units containing the same article
-        self.article_in_storagespace = Article.objects.create(lioId="1")
-        self.storageunit_in_storagespace = StorageUnit.objects.create(id="1")
-        self.article_management_service : articleManagementService = articleManagementService()
-        self.storage_management_service : storageManagementService = storageManagementService()
-        self.storagespace = StorageSpace.objects.create(id="1", storage_unit = self.storage_management_service.getStorageUnitById(id="1"), article = self.article_management_service.getArticleByLioId(lioId="1"))
-        self.storagespace = StorageSpace.objects.create(id="2", storage_unit = self.storage_management_service.getStorageUnitById(id="1"), article = self.article_management_service.getArticleByLioId(lioId="1"))
+        self.article_in_compartment = Article.objects.create(lio_id="1")
+        self.storage_in_compartment = Storage.objects.create(id="1")
+        self.article_management_service : ArticleManagementService = ArticleManagementService()
+        self.storage_management_service : StorageManagementService = StorageManagementService()
+        self.compartment = Compartment.objects.create(id="1", storage = self.storage_management_service.get_storage_by_id(id="1"), article = self.article_management_service.get_article_by_lio_id(lio_id="1"))
+        self.compartment = Compartment.objects.create(id="2", storage = self.storage_management_service.get_storage_by_id(id="1"), article = self.article_management_service.get_article_by_lio_id(lio_id="1"))
        
 
         #create a second article in third storage space but in same storage unit
-        self.article_in_storagespace = Article.objects.create(lioId="2")
-        self.storagespace = StorageSpace.objects.create(id="3", storage_unit = self.storage_management_service.getStorageUnitById(id="1"), article = self.article_management_service.getArticleByLioId(lioId="2"))
+        self.article_in_compartment = Article.objects.create(lio_id="2")
+        self.compartment = Compartment.objects.create(id="3", storage = self.storage_management_service.get_storage_by_id(id="1"), article = self.article_management_service.get_article_by_lio_id(lio_id="2"))
 
     def test_FR4_3(self):
 
         #Test that we can find/have the same article in different storage spaces in the same unit
-        storageunit = self.storage_management_service.getStorageUnitById(id="1")
-        storagespace1 = self.storage_management_service.getStorageSpaceById(id="1")
-        storagespace2 = self.storage_management_service.getStorageSpaceById(id="2")
-        article1 = self.article_management_service.getArticleByLioId("1")
-        self.assertEqual(storagespace1.article, article1)
-        self.assertEqual(storagespace2.article, article1)
-        self.assertEqual(storagespace1.storage_unit, storageunit)
-        self.assertEqual(storagespace2.storage_unit, storageunit)
+        storage = self.storage_management_service.get_storage_by_id(id="1")
+        compartment1 = self.storage_management_service.get_compartment_by_id(id="1")
+        compartment2 = self.storage_management_service.get_compartment_by_id(id="2")
+        article1 = self.article_management_service.get_article_by_lio_id("1")
+        self.assertEqual(compartment1.article, article1)
+        self.assertEqual(compartment2.article, article1)
+        self.assertEqual(compartment1.storage, storage)
+        self.assertEqual(compartment2.storage, storage)
 
 
 # Testing FR6.2 "In each storage space, the system shall record the number of a certain article based on the LIO-number"
@@ -87,32 +87,32 @@ class FR4_3_Test(TestCase):
 class FR6_2_test(TestCase):
 
     def setUp(self):
-        self.article_in_storagespace = Article.objects.create(lioId="1")
-        self.article_management_service : articleManagementService = articleManagementService()
-        self.storage_management_service : storageManagementService = storageManagementService()
-        self.storageunit_in_storagespace = StorageUnit.objects.create(id="1")
-        self.storageSpace1 = StorageSpace.objects.create(id="1", storage_unit = self.storage_management_service.getStorageUnitById(id="1"), article=self.article_management_service.getArticleByLioId(lioId="1"), amount=2)
+        self.article_in_compartment = Article.objects.create(lio_id="1")
+        self.article_management_service : ArticleManagementService = ArticleManagementService()
+        self.storage_management_service : StorageManagementService = StorageManagementService()
+        self.storage_in_compartment = Storage.objects.create(id="1")
+        self.storageSpace1 = Compartment.objects.create(id="1", storage = self.storage_management_service.get_storage_by_id(id="1"), article=self.article_management_service.get_article_by_lio_id(lio_id="1"), amount=2)
 
 
     def test_FR6_2(self):
-        test_article1 = self.article_management_service.getArticleByLioId("1")
-        test_search_storagespace = self.storage_management_service.getStorageSpaceById("1")
-        self.assertEqual(test_search_storagespace.amount, 2) 
-        self.assertNotEqual(test_search_storagespace.amount, 3) 
+        test_article1 = self.article_management_service.get_article_by_lio_id("1")
+        test_search_compartment = self.storage_management_service.get_compartment_by_id("1")
+        self.assertEqual(test_search_compartment.amount, 2) 
+        self.assertNotEqual(test_search_compartment.amount, 3) 
 
 # class FR6_2_test(TestCase):
 
 #     def setUp(self):
-#         Article.objects.create(lioId="1")
-#         StorageUnit.objects.create(id="1")
-#         StorageSpace.objects.create(id="1", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"), amount = 2)
-#         StorageSpace.objects.create(id="2", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"), amount = 4)
+#         Article.objects.create(lio_id="1")
+#         Storage.objects.create(id="1")
+#         Compartment.objects.create(id="1", storage = Storage.objects.get(id="1"), article = Article.objects.get(lio_id="1"), amount = 2)
+#         Compartment.objects.create(id="2", storage = Storage.objects.get(id="1"), article = Article.objects.get(lio_id="1"), amount = 4)
 
 #     def test_FR6_2(self):
-#         article1 = Article.objects.get(lioId="1")
-#         storagespace1 = StorageSpace.objects.get(id="1")
-#         storagespace2 = StorageSpace.objects.get(id="2")
-#         self.assertEqual(storagespace1.amount, 2) 
+#         article1 = Article.objects.get(lio_id="1")
+#         compartment1 = Compartment.objects.get(id="1")
+#         compartment2 = Compartment.objects.get(id="2")
+#         self.assertEqual(compartment1.amount, 2) 
 
 
 #Testing FR1.2
@@ -129,18 +129,18 @@ class FR6_2_test(TestCase):
 
 class FR8_9_test(TestCase): 
     def setUp(self):
-        self.article1 = Article.objects.create(lioId="1")
-        self.article2 = Article.objects.create(lioId="2")
-        self.article_management_service : articleManagementService = articleManagementService()
-        self.storage_management_service : storageManagementService = storageManagementService()
-        self.storageUnit1 = StorageUnit.objects.create(id="1")
-        self.storageUnit2 = StorageUnit.objects.create(id="2")
-        self.storageSpace1 = StorageSpace.objects.create(id="1", storage_unit = self.storage_management_service.getStorageUnitById(id="1"), article=self.article_management_service.getArticleByLioId(lioId="1"), amount=2)
-        self.storageSpace2 = StorageSpace.objects.create(id="2", storage_unit = self.storage_management_service.getStorageUnitById(id="2"), article=self.article_management_service.getArticleByLioId(lioId="2"), amount=4)
+        self.article1 = Article.objects.create(lio_id="1")
+        self.article2 = Article.objects.create(lio_id="2")
+        self.article_management_service : ArticleManagementService = ArticleManagementService()
+        self.storage_management_service : StorageManagementService = StorageManagementService()
+        self.Storage1 = Storage.objects.create(id="1")
+        self.Storage2 = Storage.objects.create(id="2")
+        self.compartment1 = Compartment.objects.create(id="1", storage = self.storage_management_service.get_storage_by_id(id="1"), article=self.article_management_service.get_article_by_lio_id(lio_id="1"), amount=2)
+        self.compartment2 = Compartment.objects.create(id="2", storage = self.storage_management_service.get_storage_by_id(id="2"), article=self.article_management_service.get_article_by_lio_id(lio_id="2"), amount=4)
 
     def test_FR8_9(self):
-        test_article1 = self.storage_management_service.searchArticleInStorage("1", "1")
-        test_article2 = self.storage_management_service.searchArticleInStorage("2", "2")
+        test_article1 = self.storage_management_service.search_article_in_storage("1", "1")
+        test_article2 = self.storage_management_service.search_article_in_storage("2", "2")
         self.assertEqual(test_article1, 2)
         self.assertEqual(test_article2, 4)
         self.assertNotEqual(test_article2, 5)
@@ -153,19 +153,19 @@ class FR8_9_test(TestCase):
 
 
 #Testing FR4.2
-#-------Fails test and gives errormessage: "StorageSpace matching query does not exist."
+#-------Fails test and gives errormessage: "Compartment matching query does not exist."
 #-------No idea what is wrong.
 # class FR4_2_test(TestCase):
 #     def setUP(self):
-#         Article.objects.create(lioId="1")
-#         StorageUnit.objects.create(id="1")
-#         StorageSpace.objects.create(id="2", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"))
-#         QRCode.objects.create(id="1", storage_space=StorageSpace.objects.get(id="2"))
+#         Article.objects.create(lio_id="1")
+#         Storage.objects.create(id="1")
+#         Compartment.objects.create(id="2", storage = Storage.objects.get(id="1"), article = Article.objects.get(lio_id="1"))
+#         QRCode.objects.create(id="1", storage_space=Compartment.objects.get(id="2"))
 
 #     def test_QRcode_containing_Storagespace(self):
-#         storagespace = StorageSpace.objects.get(id="2")
+#         compartment = Compartment.objects.get(id="2")
 #         qrcode = QRCode.objects.get(id="1")
-#         self.assertEqual(qrcode.storage_space, storagespace)
+#         self.assertEqual(qrcode.storage_space, compartment)
 
 
 
--- Web/backend/tests/test_services.py
+++ Web/backend/tests/test_services.py
@@ -1,42 +1,42 @@
 from django.test import TestCase
-from backend.dataAccess.articleAccess import articleAccess
+from backend.dataAccess.articleAccess import ArticleAccess
 from backend.coremodels.article import Article
-from backend.coremodels.storage_space import StorageSpace
-from backend.coremodels.storage_unit import StorageUnit 
+from backend.coremodels.compartment import Compartment
+from backend.coremodels.storage import Storage 
 from backend.coremodels.qr_code import QRCode
-from backend.services.articleManagementService import articleManagementService
-from backend.services.storageManagementService import storageManagementService
+from backend.services.articleManagementService import ArticleManagementService
+from backend.services.storageManagementService import StorageManagementService
 import unittest
 from unittest.mock import MagicMock
 
 class ArticleManagementServiceTest(TestCase):
     def setUp(self):
         # First we create a stub of articleAccess (this is because the method we are testing is using this data access class)
-        article_access_stub = articleAccess
-        self.article_to_search_for = Article(lioId="1") # Article to search for is created without being put in the database
-        article_access_stub.getArticleByLioId = MagicMock(return_value = self.article_to_search_for) #A return value for a method of the stub is specified (this simulates that the stub is functional, so we can test the method we are testing individually)
-        self.article_management_service : articleManagementService = articleManagementService(
+        article_access_stub = ArticleAccess
+        self.article_to_search_for = Article(lio_id="1") # Article to search for is created without being put in the database
+        article_access_stub.get_article_by_lio_id = MagicMock(return_value = self.article_to_search_for) #A return value for a method of the stub is specified (this simulates that the stub is functional, so we can test the method we are testing individually)
+        self.article_management_service : ArticleManagementService = ArticleManagementService(
             {
-                "articleAccess": article_access_stub #The mocked dependency is injected into the article management service
+                "ArticleAccess": article_access_stub #The mocked dependency is injected into the article management service
             }   # NOTE: for this to work it is important that the class we are injecting into, takes *args as an input argument for the constructor (__init__) 
         )
     def test_get_article_by_lio_id_unittestversion(self):
-        test_search = self.article_management_service.getArticleByLioId(lioId="1")
+        test_search = self.article_management_service.get_article_by_lio_id(lio_id="1")
         self.assertEqual(self.article_to_search_for, test_search)    
 
 # class storageManagementServiceTest(TestCase):    
 #     def setUp(self):                     
 #         Article.objects.create(lioId="1")
-#         StorageUnit.objects.create(id="1")
-#         StorageSpace.objects.create(id="1", storage_unit = StorageUnit.objects.get(id="1"), article = Article.objects.get(lioId="1"))
+#         storage.objects.create(id="1")
+#         Storage.objects.create(id="1", storage_unit = Storage.objects.get(id="1"), article = Article.objects.get(lioId="1"))
             
-#     # def test_getStorageUnitById(self):
-#     #     storageunit = StorageUnit.objects.get(id="1")
-#     #     self.assertEqual(storageManagementService.getStorageUnitById(self,"1"), storageunit)
+#     # def test_getStorageById(self):
+#     #     storage = Storage.objects.get(id="1")
+#     #     self.assertEqual(storageManagementService.getStorageById(self,"1"), storage)
     
-#     # def test_getStorageSpaceById(self):
-#     #     storagespace = StorageSpace.objects.get(id="1")
-#     #     self.assertEqual(storageManagementService.getStorageSpaceById(self,"1"), storagespace)
+#     # def test_getStorageById(self):
+#     #     compartment = Storage.objects.get(id="1")
+#     #     self.assertEqual(storageManagementService.getStorageById(self,"1"), compartment)
     
     
     
