1f3a4a6a4e7609f1a5c23595069f9fb4bc40e67e
Elias Leijonmarck <elile642@student.liu.se>
2022-11-09 12:34:34 +0000

    Update Web/rdxSolutionsBackendProject/urls.py

--- Web/rdxSolutionsBackendProject/urls.py
+++ Web/rdxSolutionsBackendProject/urls.py
@@ -22,8 +22,8 @@ admin.site.site_title = "Region Östergötland Portal"
 admin.site.index_title = "Welcome to Region Östergötland"
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
-    path('admin/', admin.site.urls),
+    path("api/accounts/", include("django.contrib.auth.urls")),
+    path('api/admin/', admin.site.urls),
     path('api/', include('backend.urls')),
 
 ]
