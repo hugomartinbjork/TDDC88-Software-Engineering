554d098289dd8d63e6f41a21c4af1d464a09bed0
Hugo Bjork <hugobjork@me.com>
2022-11-18 00:50:37 +0100

    Fixed all order endpoints according to api doc

--- Web/backend/serializers.py
+++ Web/backend/serializers.py
@@ -61,29 +61,6 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = GroupInfo
         fields = ('id', 'group_name')
-
-
-class OrderedArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderedArticle
-        fields = ('quantity', 'unit')
-
-
-class OrderSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Order
-        fields = ['id', 'to_storage', 'order_date',
-                  'estimated_delivery_date', 'order_state']
-
-    def create(self, validated_data):
-        ordered_articles_data = validated_data.pop('article')
-        order = Order.objects.create(**validated_data)
-        for ordered_article in ordered_articles_data:
-            OrderedArticle.objects.create(order=order, **ordered_article)
-        return order
-
-
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
@@ -145,6 +122,26 @@ class ApiArticleSerializer(serializers.ModelSerializer):
     def get_units(self, object):
         return UnitsSerializer(object).data
 
+class OrderedArticleSerializer(serializers.ModelSerializer):
+    orderedQuantity = serializers.CharField(source= 'quantity')
+    articleInfo = ApiArticleSerializer(source='article', read_only=True, many=False)
+    class Meta:
+        model = OrderedArticle
+        fields = ('articleInfo', 'orderedQuantity', 'unit')
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+ #   ord_art = serializers.SerializerMethodField('create')
+    articles = OrderedArticleSerializer(source='orderedarticle_set', read_only=True, many=True)
+    storageId = serializers.CharField(source= 'to_storage')
+    orderDate = serializers.CharField(source= 'order_date')
+    estimatedDeliveryDate = serializers.CharField(source= 'estimated_delivery_date')
+    state = serializers.CharField(source= 'order_state')
+    class Meta:
+        model = Order
+        fields = ['id', 'storageId', 'orderDate',
+                  'estimatedDeliveryDate', 'state', 'articles']
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
