d225c0613b0d3a2acec04f49b9e8128ddd435978
Elias Leijonmarck <elile642@student.liu.se>
2022-11-18 19:02:54 +0100

    Added sorting serializers for compartments

--- Web/backend/serializers.py
+++ Web/backend/serializers.py
@@ -1,6 +1,7 @@
 from dataclasses import field
 from unittest import mock
 from rest_framework import serializers
+from django.db.models import Q, Case, When, Value, IntegerField
 
 from backend.coremodels.alternative_article_name import AlternativeArticleName
 from backend.coremodels.article_has_supplier import ArticleHasSupplier
@@ -184,6 +185,22 @@ class ApiCompartmentSerializer(serializers.ModelSerializer):
                   'orderQuantityLevel', 'storageId', 'placement')
 
 
+class ApiSmallCompartmentSerializer(serializers.ModelSerializer):
+    quantity = serializers.CharField(source='amount', read_only=True)
+    qrCode = serializers.CharField(source='id', read_only=True)
+    normalOrderQuantity = serializers.IntegerField(
+        source='standard_order_amount')
+    orderQuantityLevel = serializers.IntegerField(
+        source='order_point', read_only=True)
+    storageId = serializers.PrimaryKeyRelatedField(
+        source='storage.id', read_only=True)
+
+    class Meta:
+        model = Compartment
+        fields = ('quantity', 'qrCode', 'normalOrderQuantity',
+                  'orderQuantityLevel', 'storageId', 'placement')
+
+
 class NearbyStoragesSerializer(serializers.ModelSerializer):
 
     id = serializers.PrimaryKeyRelatedField(
@@ -197,3 +214,41 @@ class NearbyStoragesSerializer(serializers.ModelSerializer):
 
     def get_self_reference(self, object):
         return ApiCompartmentSerializer(object).data
+
+
+class ArticleCompartmentProximitySerializer():
+    '''Self made serializer, contains properties 
+        article: Article, storage: Storage, is_valid(): Bool
+        and data: [ApiCompartmentModel]'''
+    def __init__(self, article: Article, storage: Storage):
+        self.article = article
+        self.storage = storage
+        self.valid = True
+        self.data = []
+        same_floor = Q(storage__floor__iexact="1")
+        same_building = Q(storage__building__iexact="1")
+
+        if (storage.floor is None):
+            self.valid = False
+        if (storage.building is None):
+            self.valid = False
+        if (not self.valid):
+            return
+
+        nearest_comps = article.compartment_set.all().annotate(
+            proximity_ordering=Case(
+                When(same_building & same_floor, then=Value(2)),
+                When(same_building & ~same_floor, then=Value(1)),
+                When(same_floor & ~same_building, then=Value(0)),
+                When(~same_floor & ~same_building, then=Value(-1)),
+                output_field=IntegerField(),
+            )
+        ).order_by('-proximity_ordering')
+        if (not nearest_comps):
+            self.valid = False
+        
+        self.data = ApiSmallCompartmentSerializer(
+            nearest_comps, many=True, read_only=True).data
+
+    def is_valid(self):
+        return self.valid
