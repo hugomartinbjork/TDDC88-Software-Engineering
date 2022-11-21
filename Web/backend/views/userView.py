48fe57c05ac128b1787913c21ad51f55e1020aa1
Jakob Berggren <jakbe841@student.liu.se>
2022-11-21 15:02:00 +0100

    implemented functionality to get all users. Not done yet

--- Web/backend/views/userView.py
+++ Web/backend/views/userView.py
@@ -1,11 +1,38 @@
 from backend.__init__ import serviceInjector as si
 from backend.services.userService import UserService
 from rest_framework.views import APIView
-from django.http import Http404, JsonResponse, HttpResponseBadRequest
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404, HttpResponseBadRequest
 from ..serializers import UserInfoSerializer
 
 
 class User(APIView):
+    '''View for getting all users and posting a user to the DB. 
+    Should only be accessable by admins'''
+
+    @si.inject
+    def __init__(self, _deps, *args):
+        self.userService: UserService = _deps['UserService']()
+
+    def get(self, request):
+        '''Get all users from the database. Should Only accessable by admins'''
+
+        users = self.userService.get_users()
+        if users is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = UserInfoSerializer(users, many=True)
+        if serializer.is_valid:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request):
+        '''posts a user to the database. Should Only accessable by Admins'''
+        pass
+
+
+class UserId(APIView):
     '''User'''
     @si.inject
     def __init__(self, _deps, *args):
@@ -23,5 +50,5 @@ class User(APIView):
                 raise Http404("Could not find user")
             serializer = UserInfoSerializer(user)
             if serializer.is_valid:
-                return JsonResponse(serializer.data, safe=False, status=200)
+                return Response(serializer.data, safe=False, status=200)
         return HttpResponseBadRequest
