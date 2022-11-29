3d2fbb283507e61f10e442fd1761823820967d95
guski416 <guski416@tlvm-4-1-3.ad.liu.se>
2022-11-29 13:24:42 +0100

    minor adjustments

--- Web/populate.py
+++ Web/populate.py
@@ -3,10 +3,12 @@ from sqlalchemy import create_engine
 import psycopg2
 
 
-'''Engine with deployed postgreSQL-database'''
-engine = create_engine(
-"postgresql://admin:rdxsolutions@postgres-service:5432/postgresdb")
+# '''Engine with deployed postgreSQL-database'''
+# engine = create_engine(
+# "postgresql://admin:rdxsolutions@postgres-service:5432/postgresdb")
 
+'''Engine with local SQLite-database'''
+engine = create_engine("sqlite:///mydatabase1")
 
 
 with pd.ExcelFile('db_data/groupinfo.xlsx') as xls:
@@ -18,6 +20,12 @@ with pd.ExcelFile('db_data/costcenter.xlsx') as xls:
     df = pd.read_excel(xls)
     df.to_sql(name='backend_costcenter', con=engine,
               if_exists='append', index=False)
+
+with pd.ExcelFile('db_data/supplier.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_supplier', con=engine,
+              if_exists='append', index=False)
+
 
 with pd.ExcelFile('db_data/testarticles.xlsx') as xls:
     df = pd.read_excel(xls)
