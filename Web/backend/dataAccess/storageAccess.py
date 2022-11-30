8b9070e175db99108111ef45ebcb9627367b0be2
Jakob Berggren <jakbe841@student.liu.se>
2022-11-30 11:40:00 +0100

    experiment with prefetch

--- Web/backend/dataAccess/storageAccess.py
+++ Web/backend/dataAccess/storageAccess.py
@@ -63,7 +63,7 @@ class StorageAccess():
     def get_all_storages(self) -> dict:
         '''Returns every storage unit.'''
         try:
-            return Storage.objects.filter().prefetch_related()
+            return Storage.objects.filter().prefetch_related('compartment_set')
         except Exception:
             return None
 
