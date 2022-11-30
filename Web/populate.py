7872c6d422df20326615ea8e168916ccbe26fadb
guski416 <guski416@tlvm-4-1-3.ad.liu.se>
2022-11-30 16:06:06 +0100

    try again

--- Web/populate.py
+++ Web/populate.py
@@ -16,31 +16,31 @@ with pd.ExcelFile('db_data/groupinfo.xlsx') as xls:
     df.to_sql(name='backend_group', con=engine,
               if_exists='append', index=False)
 
-# with pd.ExcelFile('db_data/costcenter.xlsx') as xls:
-#     df = pd.read_excel(xls)
-#     df.to_sql(name='backend_costcenter', con=engine,
-#               if_exists='append', index=False)
+with pd.ExcelFile('db_data/costcenter.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_costcenter', con=engine,
+              if_exists='append', index=False)
 
-# with pd.ExcelFile('db_data/supplier.xlsx') as xls:
-#     df = pd.read_excel(xls)
-#     df.to_sql(name='backend_supplier', con=engine,
-#               if_exists='append', index=False)
+with pd.ExcelFile('db_data/supplier.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_supplier', con=engine,
+              if_exists='append', index=False)
 
 
-# with pd.ExcelFile('db_data/testarticles.xlsx') as xls:
-#     df = pd.read_excel(xls)
-#     df.to_sql(name='backend_article', con=engine,
-#               if_exists='append', index=False)
+with pd.ExcelFile('db_data/testarticles.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_article', con=engine,
+              if_exists='append', index=False)
 
-# with pd.ExcelFile('db_data/storages.xlsx') as xls:
-#     df = pd.read_excel(xls)
-#     df.to_sql(name='backend_storage', con=engine,
-#               if_exists='append', index=False)
+with pd.ExcelFile('db_data/storages.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_storage', con=engine,
+              if_exists='append', index=False)
 
-# with pd.ExcelFile('db_data/compartments.xlsx') as xls:
-#     df = pd.read_excel(xls)
-#     df.to_sql(name='backend_compartment', con=engine,
-#               if_exists='append', index=False)
+with pd.ExcelFile('db_data/compartments.xlsx') as xls:
+    df = pd.read_excel(xls)
+    df.to_sql(name='backend_compartment', con=engine,
+              if_exists='append', index=False)
 
 with pd.ExcelFile('db_data/transactions.xlsx') as xls:
     df = pd.read_excel(xls)
